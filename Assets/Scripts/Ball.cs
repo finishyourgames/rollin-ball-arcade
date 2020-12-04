326a149c157079282adf81df9643b39551d5dc19
finishyourgames <75401162+finishyourgames@users.noreply.github.com>
2020-12-04 03:20:30 +0000

    Game Complete

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Ball : MonoBehaviour
+{
+	[SerializeField] float _speed = 5f;
+	[SerializeField] UnityEvent OnScoreUpdated;
+	[SerializeField] ParticleSystem _collectParticles;
+
+	Rigidbody _rigidbody;
+	Vector3 _input;
+	
+	public int Score;
+	[SerializeField] private float _oppositeSpeed = 10f;
+
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		_input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+	}
+
+	private void FixedUpdate()
+	{
+		var direction = _input;
+		if (Mathf.Sign(_rigidbody.velocity.x) != Mathf.Sign(_input.x))
+			direction.x *= _oppositeSpeed;
+		if (Mathf.Sign(_rigidbody.velocity.z) != Mathf.Sign(_input.y))
+			direction.z *= _oppositeSpeed;
+
+		_rigidbody.AddForce(direction.normalized * _speed);
+	}
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (!other.CompareTag("Collectible")) return;
+
+		transform.DOScale(Vector3.one, .2f).From(Vector3.one * 1.2f);
+		Instantiate(_collectParticles, other.transform.position, Quaternion.identity);
+
+		Destroy(other.gameObject);
+
+		Score++;
+		OnScoreUpdated?.Invoke();
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField] Text _scoreText;
+	private Ball _ball;
+	[SerializeField] private GameObject _completePanel;
+
+	private void Start()
+	{
+		_ball = FindObjectOfType<Ball>();
+		UpdateScore();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			SceneManager.LoadScene(0);
+		}
+	}
+
+	public void UpdateScore()
+	{
+		_scoreText.text = _ball.Score.ToString();
+		_scoreText.rectTransform.DOScale(Vector3.one, .2f).From(Vector3.one * 1.2f);
+
+		StartCoroutine(CheckWinRoutine());
+
+		
+	}
+
+	private IEnumerator CheckWinRoutine()
+	{
+		yield return new WaitForSeconds(.2f);
+		var collectibles = GameObject.FindGameObjectsWithTag("Collectible");
+		if (collectibles.Length > 0)
+		{
+
+		}
+		else
+		{
+			CompleteGame();
+		}
+
+	}
+
+	public void CompleteGame()
+	{
+		_completePanel.SetActive(true);
+		Time.timeScale = 0;
+	}
+}
